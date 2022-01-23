895ba8306070673606972521b5a45afd8213470f
manepallilakshmi <97447403+manepallilakshmi@users.noreply.github.com>
2022-01-23 22:12:02 +0530

    #include<stdio.h> #include<math.h>
    int main()
    {
        int a,b;
        float average;
        
        scanf("%d%d",&a,&b);
        
        
        
        average= (float)(a+b)/2 ;
        printf("%.4f",average);
    }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a,b;
+    float average;
+    
+    scanf("%d%d",&a,&b);
+    
+    
+    
+    average= (float)(a+b)/2 ;
+    printf("%.4f",average);
+}

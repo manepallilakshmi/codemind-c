aac10a0096e286ae69facfde5067eb0d2f96ff7c
manepallilakshmi <97447403+manepallilakshmi@users.noreply.github.com>
2022-05-02 00:18:24 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d", &n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",64+i);         }         printf(" ");         j++;     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,j,i;
-    scanf("%d",&n);
+    int n,i,j;
+    scanf("%d", &n);
     for(i=1;i<=n;i++)
     {
-        for(j=n;j>=1;j--)
+        for(j=1;j<=n;j++)
         {
-            printf("%d ",j);
+            printf("%c ",64+i);
         }
         printf("
 ");

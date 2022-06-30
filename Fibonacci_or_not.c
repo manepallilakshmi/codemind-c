5cd3684c5cc617388cd2ef1fffc5903a6ea2e915
manepallilakshmi <97447403+manepallilakshmi@users.noreply.github.com>
2022-06-30 22:52:00 +0530

    #include<stdio.h> int main() {     int a,b,c,next,n;     scanf("%d",&n);     if((n==0) || (n==1))     {         printf("%d is fibonacci term",n);     }     else     {         a=0;         b=1;         c=a+b;         while(c<n)         {             a=b;             b=c;             c=a+b;         }         if(c==n)         {             printf("True");         }         else         {             printf("False");         }     } }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,next,n;
+    scanf("%d",&n);
+    if((n==0) || (n==1))
+    {
+        printf("%d is fibonacci term",n);
+    }
+    else
+    {
+        a=0;
+        b=1;
+        c=a+b;
+        while(c<n)
+        {
+            a=b;
+            b=c;
+            c=a+b;
+        }
+        if(c==n)
+        {
+            printf("True");
+        }
+        else
+        {
+            printf("False");
+        }
+    }
+}
